0fb8f109181704e7fc6f69819f8c4c6a3e5e5833
shiryu ueno <ueno@cv.info.gifu-u.ac.jp>
2022-12-21 01:16:06 +0000

    update

--- materials/get_brain_data.py
+++ materials/get_brain_data.py
@@ -9,23 +9,23 @@ reconstruct to .png data.
 '''
 
 # If there is dataset already, skip this code
-if os.path.exists('downloaded_data/BrainTumor/1'):
+if os.path.exists('../downloaded_data/BrainTumor/1'):
     print('BurainTumor data may exist, please confirm')
     pass
 
 else:
     # read mat file
-    cross_validation_index = h5py.File('downloaded_data/brain_mat/cvind.mat', 'r')
+    cross_validation_index = h5py.File('../downloaded_data/brain_mat/cvind.mat', 'r')
     cross_validation_index = np.array(cross_validation_index['cvind'])
 
     classes = ['meningioma', 'glioma', 'pituitary']
     num_each_class = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
     for i in range(1, 6):
         for j in classes:
-            os.makedirs(os.path.join('downloaded_data/BrainTumor', str(i), j), exist_ok=True)
+            os.makedirs(os.path.join('../downloaded_data/BrainTumor', str(i), j), exist_ok=True)
 
     for i in range(1, len(cross_validation_index[0])+1):
-        f = h5py.File('downloaded_data/brain_mat/'+str(i)+'.mat', 'r')
+        f = h5py.File('../downloaded_data/brain_mat/'+str(i)+'.mat', 'r')
         label = f.get('cjdata/label')
         image_raw = f.get('cjdata/image')
         nd_image = np.array(image_raw)
@@ -37,6 +37,6 @@ else:
         nd_image = (nd_image / (2**8-1))*255
 
         num_each_class[label][int(cross_validation_index[0][i-1])-1] += 1
-        save_path = os.path.join('downloaded_data/BrainTumor', str(int(cross_validation_index[0][i-1])), classes[label])
+        save_path = os.path.join('../downloaded_data/BrainTumor', str(int(cross_validation_index[0][i-1])), classes[label])
         pl_img = Image.fromarray(nd_image).convert('L')
         pl_img.save(os.path.join(save_path, str(num_each_class[label][int(cross_validation_index[0][i-1])-1])+'.png'))
--- materials/get_gaps.py
+++ materials/get_gaps.py
@@ -11,31 +11,31 @@ Then, way how to utilize GAPs dataset is shared by official library, so please r
 '''
 
 # download data
-if os.path.exists('downloaded_data/GAPs/v2/ZEB_50k_64'):
+if os.path.exists('../downloaded_data/GAPs/v2/ZEB_50k_64'):
     print('GAPs data may already downloaded')
     pass
 else:
-    os.makedirs('downloaded_data/GAPs', exist_ok=True)
+    os.makedirs('../downloaded_data/GAPs', exist_ok=True)
     gaps.download(login='gapsro2s;i2A*7',
-                output_dir='downloaded_data/GAPs', 
+                output_dir='../downloaded_data/GAPs', 
                 version=2,
                 patchsize=224, 
                 issue='ZEB_50k')
 
 
 # prepare png data
-if not os.path.exists('downloaded_data/GAPs/v2/train/Potholes'):
+if not os.path.exists('../downloaded_data/GAPs/v2/train/Potholes'):
     print('GAPs train_dataset may be ready')
     pass
 else:
     cnt = [0, 0, 0, 0, 0, 0]
     classes = ['Intact_road', 'Applied_patches', 'Potholes', 'Inlaid_patches', 'Open_joints', 'Cracks']
     for i in classes:
-        os.makedirs(os.path.join('downloaded_data/GAPs/v2/train', i), exist_ok=True)
+        os.makedirs(os.path.join('../downloaded_data/GAPs/v2/train', i), exist_ok=True)
 
 
-    x_train0, y_train0 = gaps.load_chunk(chunk_id=0, patchsize=224, issue='ZEB_50k', subset='train', datadir='downloaded_data/GAPs')
-    x_train1, y_train1 = gaps.load_chunk(chunk_id=1, patchsize=224, issue='ZEB_50k', subset='train', datadir='downloaded_data/GAPs')
+    x_train0, y_train0 = gaps.load_chunk(chunk_id=0, patchsize=224, issue='ZEB_50k', subset='train', datadir='../downloaded_data/GAPs')
+    x_train1, y_train1 = gaps.load_chunk(chunk_id=1, patchsize=224, issue='ZEB_50k', subset='train', datadir='../downloaded_data/GAPs')
     x_train = np.concatenate([x_train0, x_train1])
     y_train = np.concatenate([y_train0, y_train1])
     import pdb
@@ -50,20 +50,20 @@ else:
         img = Image.fromarray(img_array)
 
         cnt[int(y_train[i])] += 1
-        img_path = 'downloaded_data/GAPs/v2/train/' + classes[int(y_train[i])] + '/' + str(cnt[int(y_train[i])])
+        img_path = '../downloaded_data/GAPs/v2/train/' + classes[int(y_train[i])] + '/' + str(cnt[int(y_train[i])])
         img.save(img_path+'.png')
 
-if os.path.exists('downloaded_data/GAPs/v2/test/Potholes'):
+if os.path.exists('../downloaded_data/GAPs/v2/test/Potholes'):
     print('GAPs test_dataset may be ready')
     pass
 else:
     cnt = [0, 0, 0, 0, 0, 0]
     classes = ['Intact_road', 'Applied_patches', 'Potholes', 'Inlaid_patches', 'Open_joints', 'Cracks']
     for i in classes:
-        os.makedirs(os.path.join('downloaded_data/GAPs/v2/test', i), exist_ok=True)
+        os.makedirs(os.path.join('../downloaded_data/GAPs/v2/test', i), exist_ok=True)
 
 
-    x_test, y_test = gaps.load_chunk(chunk_id=0, patchsize=224, issue='ZEB_50k', subset='test', datadir='downloaded_data/GAPs')
+    x_test, y_test = gaps.load_chunk(chunk_id=0, patchsize=224, issue='ZEB_50k', subset='test', datadir='../downloaded_data/GAPs')
 
     for i in range(len(x_test)):
         nd_array = x_test[i][0]
@@ -73,6 +73,6 @@ else:
         img = Image.fromarray(img_array)
 
         cnt[int(y_test[i])] += 1
-        img_path = 'downloaded_data/GAPs/v2/test/' + classes[int(y_test[i])] + '/' + str(cnt[int(y_test[i])])
+        img_path = '../downloaded_data/GAPs/v2/test/' + classes[int(y_test[i])] + '/' + str(cnt[int(y_test[i])])
         img.save(img_path+'.png')
         print(i+1)
